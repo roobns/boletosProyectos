1f9fdc4c26c12870470dc3c8ca9ff353113fa8e8
roobns <roobns@gmail.com>
2017-01-18 00:40:50 -0600

    cambios login

--- src/script/controllers/loginCtrl.js
+++ src/script/controllers/loginCtrl.js
@@ -1,10 +1,21 @@
 'use strict';
 
-module.exports = function($scope, $filter,dataServices, callRestFactory, errorMessageHandler) {
+module.exports = function($scope, $http, $filter,dataServices, callRestFactory, errorMessageHandler) {
 
 $scope.init = function(){
 
 };
+
+$scope.login = function(){
+	var data= 'email=' + $scope.email + '&pwds=' + $scope.password;
+	$http.post('http://celebrausana.com/celebra-back/login', data )
+	.success(function(data, status, headers, config) {
+		$scope.message = data;
+	})
+	.error(function(data, status, headers, config) {
+		alert( "failure message: " + JSON.stringify({data: data}));
+	});
+}
 
 
   $scope.init();
