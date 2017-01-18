3d2bbb41754818dd6c1fe48c4b179a701a82f783
roobns <roobns@gmail.com>
2017-01-18 01:46:29 -0600

    servicio login

--- src/script/controllers/loginCtrl.js
+++ src/script/controllers/loginCtrl.js
@@ -6,16 +6,35 @@ $scope.init = function(){
 
 };
 
-$scope.login = function(){
-	var data= 'email=' + $scope.email + '&pwds=' + $scope.password;
-	$http.post('http://celebrausana.com/celebra-back/login', data )
-	.success(function(data, status, headers, config) {
-		$scope.message = data;
-	})
-	.error(function(data, status, headers, config) {
-		alert( "failure message: " + JSON.stringify({data: data}));
-	});
-}
+
+
+
+$scope.login = function () {
+           // use $.param jQuery function to serialize data from JSON 
+            var data = $.param({
+                email: $scope.email,
+                pwd: $scope.pwd
+            });
+        
+            var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+
+            $http.post('http://celebrausana.com/celebra-back/login', data, config)
+            .success(function (data, status, headers, config) {
+                $scope.PostDataResponse = data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+        };
+
+    
 
 
   $scope.init();
